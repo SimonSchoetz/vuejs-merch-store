a796d7e1ef012eca8df5f6dce994346ef51105cb
Simon <simonsch.tz@gmail.com>
2020-09-07 11:22:13 +0200

    Start project with scss

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
+
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <Head msg="Busted Fingerz Merch Store" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Head from './components/Head.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Head
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "./style.scss"
+
 </style>
