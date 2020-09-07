84e2522cd8aef0679adfb2c053b847df3c74cc02
Simon <simonsch.tz@gmail.com>
2020-09-07 16:03:44 +0200

    Getting the right data out of the db

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,7 +1,9 @@
 
 <template>
 <div class="product">
-    <h2>This is {{merchStock.product}}</h2>
+    <h2>{{productName}}s</h2>
+    <img width="200" :src="productTypes[index].img" />
+    <button @click="clg(productTypes)">clg</button>
 </div>  
 </template>
 
@@ -9,8 +11,21 @@
 export default {
     name: "ProductCard",
     props: [
-        "merchStock"
+        "merchStock",
     ],
+    data() {
+        return {
+        products: this.merchStock,
+        productName: this.merchStock.product,
+        productTypes: this.merchStock.types,
+        index: 0
+        }
+    },
+    methods: {
+        clg(input) {
+            return console.log(input)
+        }
+    }
 }
 </script>
 
