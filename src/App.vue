00dd706194be49a4bb064bfd3fd48aa517b2af83
Simon <simonsch.tz@gmail.com>
2020-09-07 14:27:47 +0200

    Make product show on Product Card

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,5 @@ export default {
 </script>
 
 <style lang="scss">
-@import "./style.scss"
-
+  @import "./style.scss"
 </style>
--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,24 +1,23 @@
 <template>
   <div class="products">
     <h1>Products</h1>
-    
+    <Product :merch-stock="stock" />
   </div>
 </template>
 
 <script>
-import merchDB from "../merch.json"
-
+import merchDB from "../merch.json";
+import Product from  "./Product"
+console.log(merchDB)
 export default {
   name: 'Products',
-  data() {
-       return {
-           stock: merchDB,
-    }
-  },
+  data() { return {
+        stock: merchDB
+  }},
   methods: {
   },
-  props: {
-    // msg: String
+  components: {
+      Product
   }
 }
 </script>
