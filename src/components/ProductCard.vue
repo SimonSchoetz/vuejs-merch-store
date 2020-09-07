b530bd269e06f333fffba1330014dc232e1a0b25
Simon <simonsch.tz@gmail.com>
2020-09-07 18:56:37 +0200

    Finish color palette for today

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -3,14 +3,12 @@
 <div class="product">
     <h2>{{productName}}s</h2>
     <div> 
-        <img width="200" :src="productTypes[index].img" />
+        <img width="200" :src="productTypes[selectedType].img" />
         <div class="color-container" v-for="(color, i) in colors()" :style="{backgroundColor:color}" :key="i">
-            <div @mouseover="setID(type.id)" class="main-color" v-for="(type, i) in productTypes" v-show="type.colorMain === color" :style="{backgroundColor:type.colorMain}" :key="i"> 
-                    <div class="color-logo-1" :style="{backgroundColor:type.colorLogo1}"> 
-                        <div class="color-logo-2" v-show="type.colorLogo2" :style="{backgroundColor:type.colorLogo2}"></div>
-                    </div>
-                
-                
+            <div @mouseover="updateProduct(type.id)" class="main-color" v-for="(type, i) in productTypes" v-show="type.colorMain === color" :style="{backgroundColor:type.colorMain}" :key="i"> 
+                <div class="color-logo-1" :style="{backgroundColor:type.colorLogo1}"> 
+                    <div class="color-logo-2" v-show="type.colorLogo2" :style="{backgroundColor:type.colorLogo2}"></div>
+                </div>
             </div>
 
         </div>
@@ -32,7 +30,7 @@ export default {
             products: this.merchStock,
             productName: this.merchStock.product,
             productTypes: this.merchStock.types,
-            index: 0,
+            selectedType: 0,
             colors: () => {
                     let colors = [];
                     this.merchStock.types.map(type => {
@@ -47,8 +45,8 @@ export default {
         clg(input) {
             return console.log(input)
         },
-        setID(id) {
-            return this.index = id
+        updateProduct(id) {
+            return this.selectedType = id
         }
     },
     // computed: {
