75ffdb6c510e04043bbd3456c807e55a5e15156a
Simon <simonsch.tz@gmail.com>
2020-09-08 13:40:52 +0200

    Color and quantity show up when hovered and focus

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -2,20 +2,36 @@
 <template>
 <div class="product">
     <h2>{{productName}}s</h2>
-    <div class="product-card"> 
+    <form class="product-card"> 
         <img width="400" :src="productTypes[selectedType].img" />
-        <div> 
-            <div class="color-container" @mouseenter="showColorPalette(color)" @mouseleave="showColorPalette(false)" v-for="(color, i) in colors()" :key="i">
-                
-                <div class="color-cover" :class="{hideColors: color === currColor}" :style="{backgroundColor:color}" > </div>
-                <div class="main-color" :class="{hideColors: color !== currColor}" @mouseover="updateProduct(i)" v-for="(type, i) in productTypes" v-show="type.colorMain === color" :style="{backgroundColor:type.colorMain}" :key="i"> 
-                    <div class="color-logo-1" :class="{hideColors: color !== currColor}" :style="{backgroundColor:type.colorLogo1}"> 
-                        <div class="color-logo-2" :class="{hideColors: color !== currColor}" v-show="type.colorLogo2" :style="{backgroundColor:type.colorLogo2}"></div>
-                    </div>
+        <div class="color-picker">  
+            <div 
+            class="color-container" 
+            @mouseenter="showColorPalette(color)" 
+            @mouseleave="showColorPalette(false)" v-for="(color, i) in colors()" 
+            :key="i">
+                <div class="color-cover" :class="{hideColors: color === currColor}" :style="{backgroundColor:color}" > 
                 </div>
+                <button 
+                class="main-color"
+                @focus="updateProduct(color, i)"
+                @blur="showColorPalette(false)"
+                :class="{hideColors: color !== currColor}" 
+                @mouseover="updateProduct(color, i)" v-for="(type, i) in productTypes" 
+                v-show="type.colorMain === color" 
+                :style="{backgroundColor:type.colorMain}" 
+                :key="i"> 
+                    <div class="color-logo-1" :class="{hideColors: color !== currColor}" :style="{backgroundColor:type.colorLogo1}"> 
+                        <div class="color-logo-2" :class="{hideColors: color !== currColor}" v-show="type.colorLogo2" :style="{backgroundColor:type.colorLogo2}" />
+                    </div>
+                </button>
             </div>
         </div>
-    </div>
+        <div class="product-details"> 
+            <p>Quantity: {{productTypes[selectedType].quantity}}
+            </p>
+        </div>
+    </form>
     
     <button @click="clg(currColor)">clg</button>
 </div>  
@@ -48,8 +64,11 @@ export default {
         clg(input) {
             return console.log(input)
         },
-        updateProduct(id) {
-            return this.selectedType = id
+        updateProduct(color, id) {
+            return (
+                this.selectedType = id,
+                this.currColor = color
+            )
         },
         showColorPalette(input) {
             input ? this.currColor = input : this.currColor = "initial"
