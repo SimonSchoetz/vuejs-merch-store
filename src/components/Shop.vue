c8043c2f732ed786af4a060e4f8e3349a0c11ba4
Simon <simonsch.tz@gmail.com>
2020-09-09 10:13:19 +0200

    Add shop.vue to git stack

---
+++ src/components/Shop.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="Shop">
+    <h1>Shop</h1>
+        <ProductCard v-for="(product, i) in stock" :key="i" :merch-stock="product" />
+  </div>
+</template>
+
+<script>
+import merchDB from "../merch.json";
+import ProductCard from  "./ProductCard"
+export default {
+  name: 'Shop',
+  data() { return {
+        stock: merchDB
+  }},
+  methods: {
+  },
+  components: {
+      ProductCard
+  }
+}
+</script>
+
